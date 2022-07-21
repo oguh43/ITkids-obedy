9957d432699b4606e49287a2e8a030d11ef17b7d
oguh43 <agenthugo43@gmail.com>
2022-07-21 09:36:16 +0200

    fml

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
-const uaup = require('uaup-js');
+const uaup = require('./uaup-js.js');
 
 
 //This is Optional
@@ -12,16 +12,23 @@ const defaultStages = {
     Launch: "Launching..." // When Launching the Application.
 };
 
-const updateOptions = {
+let updateOptions = {
     gitRepo: "ITkids-obedy", // [Required] Your Repo Name
     gitUsername: "oguh43",  // [Required] Your GitHub Username.
 
-    appName: "ITkids_obedy", //[Required] The Name of the app archive and the app folder.
+    appName: "itkids_obedy", //[Required] The Name of the app archive and the app folder.
     appExecutableName: "ITkids obedy.exe", //[Required] The Executable of the Application to be Run after updating.
 
     progressBar: document.getElementById("download"), // {Default is null} [Optional] If Using Electron with a HTML Progressbar, use that element here, otherwise ignore
     label: document.getElementById("download-label"), // {Default is null} [Optional] If Using Electron, this will be the area where we put status updates using InnerHTML
     stageTitles: defaultStages, // {Default is defaultStages} [Optional] Sets the Status Title for Each Stage
 };
+if (process.platform === "win32") {
+    updateOptions.appName = "ITkids_obedy_win32"
+    updateOptions.appExecutableName = "itkids_obedy.exe"
+}else{
+    updateOptions.appName = "ITkids_obedy_linux"
+    updateOptions.appExecutableName = "itkids_obedy"
+}
 
 uaup.Update(updateOptions);
